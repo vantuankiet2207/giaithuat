da434d4a2d44bf32abfef8332e1becbadad24db9
anhphuongdo <89734460+anhphuongdo@users.noreply.github.com>
2021-12-11 23:13:49 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections;
-//helooooooo everybode
-yyeuiiooueyetwwttwue;
-sdfghjkjhgfdsdfghjkjhgfdsdfg;
 class program
 {
     static void Main(string[] args)
